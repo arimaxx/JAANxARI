614f68f1d7b82958e07dd91666bde4c0c0401cfa
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-04-26 07:14:02 +0530

    Update newtag.py

--- AarohiX/plugins/maxa/newtag.py
+++ AarohiX/plugins/maxa/newtag.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import random
-
+from config import API_ID, API_HASH
 # Your session file path
 session_file_path = 'BQG_NJ0AJTqTf8Md8F9U-y5GLXnGfa8N1QEks4FBIQvJA5GR3UqCevNKvbHKRiXhGkwsnsqsS5YlMgTfPkTEEULHKC_9phxxX16I8OqYIwWLDbKgyLWr35d9BbFaG6zBK95WLq976ukmaxHmFVPr-WcU7lr1t69VJkE5AWVHZ7QnpPivCiNCPKlgx2Zd2dGcKpPKvoU2LjybZArTjUxoyI6axjgbNRdQ95LSx8aHWJpVZFNqPMB2PpI_MgaAkxh9o0nMojBu2LpyPfrn78i-MLz3E54MWCfngn_VHrdtUTPPhroDmaUI_hqjENXJJdG_kuG4KQ0FMZ5TuQifUAwWAODCYbeatQAAAAGF6z4UAA'
 
@@ -12,15 +12,15 @@ messages = [
 ]
 
 # Initialize Pyrogram client
-app = Client("my_session", api_id=API_ID, api_hash=API_HASH)
+app = Client("my_session", api_id=API_ID, api_hash=API_HASH, session_string=session_file_path)
 
 # Command to trigger the sending of random morning messages
 @app.on_message(filters.command("morning"))
 def send_morning_messages(client, message):
-    chat = client.resolve_peer('target_group_username')
+    chat = cclient.get_chat('target_group_username')
     participants = client.get_chat_members(chat.chat_id)
 
     for member in participants:
         if member.user.username:
             random_message = random.choice(messages).format(member.user.username)
-            client.send_message(chat.chat_id, random_message)
+            client.send_message(chat.id, random_message)
